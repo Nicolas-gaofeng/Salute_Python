8f0804a9e64a01e30a6b7744d61674483558f04f
zgf <396607662@qq.com>
2020-12-28 11:15:33 +0800

    add file

---
+++ 02_python_basic/01_first_python/HelloWorld.py
@@ -0,0 +1,16 @@
+#!/usz/bin/env python
+# -*- coding:utf-8 -*-
+
+
+# =========================================  说明 =========================================
+# print 是 python 中我们学习的第一个 函数
+# print 函数的作用，可以把 "" 内部的内容，输出到屏幕上
+# 右键选择run或者右上角绿色三角执行
+
+flag = True
+# flag = False
+if flag:
+    print("Hello Python")
+    print("你好， 世界")
+    print("Hello Hello")
+    print("hello")
